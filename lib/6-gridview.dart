f5a9e2ce222552da09f05d0d5be26bc52eab6ae6
lidongliang <dongfang369>
2019-04-25 13:12:41 +0800

    1

--- lib/6-gridview.dart
+++ lib/6-gridview.dart
@@ -7,10 +7,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: Text("ListView实例"),
-            ),
-            body:
+      appBar: AppBar(
+        title: Text("ListView实例"),
+      ),
+      body:
 //      GridView.count(
 //          crossAxisCount: 2,
 //          //scrollDirection: Axis.horizontal,
@@ -25,24 +25,29 @@ class MyApp extends StatelessWidget {
 //              ),
 //            );
 //          })),
-                GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 10,
-                    ),
-                    itemBuilder: (context, index) {
-                      print('$index');
-                      return Container(
-                        color: Colors.blueAccent,
-                        margin: EdgeInsets.all(10.0),
-                        child: Text(
-                          '$index',
-                          style: TextStyle(
-                            color: Colors.red,
-                            fontSize: 26,
-                          ),
-                        ),
-                      );
-                    })));
+          Container(
+        margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                childAspectRatio: 2 / 1),
+            itemBuilder: (context, index) {
+              print('$index');
+              return Container(
+                color: Colors.blueAccent,
+//                        margin: EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
+                child: Text(
+                  '$index',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 26,
+                  ),
+                ),
+              );
+            }),
+      ),
+    ));
   }
 }
