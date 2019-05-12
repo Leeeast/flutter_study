d5e790a5c1e406024cdf358eed5996f8fd508811
Leeeast <lidongliang_1990@126.com>
2019-05-13 01:14:18 +0800

    3d

--- lib/animation/5-3d.dart
+++ lib/animation/5-3d.dart
@@ -9,12 +9,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Offset _offset = Offset.zero;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Transform(
-          transform: Matrix4.identity(),
+          transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.001)
+          ..rotateX(_offset.dx*0.01)
+          ..rotateY(_offset.dy*0.01)
+          ..rotateZ(_offset.dy*0.01),
+          child: GestureDetector(
+            onPanUpdate: (detail){
+                setState(() {
+                  _offset += detail.delta;
+                });
+            },
+            onDoubleTap: (){
+              setState(() {
+                _offset = Offset.zero;
+              });
+            },
+            child: Container(
+              width: 200.0,
+              height: 200.0,
+              color: Colors.blue,
+            ),
+          ),
         ),
       ),
     );
