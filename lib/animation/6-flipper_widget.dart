747bb7b7abf83b9283b5a5ff28756ebab80afd79
Leeeast <lidongliang_1990@126.com>
2019-05-14 05:15:36 +0800

    flipper

--- lib/animation/6-flipper_widget.dart
+++ lib/animation/6-flipper_widget.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage>
   void initState() {
     super.initState();
     _animationController =
-        AnimationController(vsync: this, duration: Duration(seconds: 1));
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
     _animation = TweenSequence(
       [
         TweenSequenceItem(
@@ -28,19 +28,90 @@ class _HomePageState extends State<HomePage>
           weight: 0.5,
         ),
         TweenSequenceItem(
-          tween: Tween(begin: pi/2,end: 0.0),
+          tween: Tween(begin: pi / 2, end: 0.0),
           weight: 0.5,
         ),
       ],
     ).animate(_animationController);
   }
+
   @override
   void dispose() {
     _animationController.dispose();
   }
 
+  _doAnim() {
+    reversed ? _animationController.reverse() : _animationController.forward();
+    reversed = !reversed;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (BuildContext context, Widget child) {
+            return Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..rotateY(_animation.value),
+              alignment: Alignment.center,
+              child: GestureDetector(
+                onTap: _doAnim,
+                child: IndexedStack(
+                  children: <Widget>[
+                    CardOne(),
+                    CardTwo(),
+                  ],
+                  alignment: Alignment.center,
+                  index: _animationController.value < 0.5 ? 0 : 1,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CardOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.redAccent,
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        child: Text(
+          '点我看密码',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.blueAccent,
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        child: Text(
+          '123456',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
   }
 }
